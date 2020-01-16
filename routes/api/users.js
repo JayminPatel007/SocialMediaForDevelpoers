2408b724e5137c296fe053783a0a26d9df7f4a05
discussion4all <discussion4all@yahoo.com>
2020-01-16 16:17:53 +0530

    Now Sending jwt token after signup

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../../models/User");
 
@@ -48,7 +50,16 @@ router.post("/",[
 
         await user.save();
 
-        res.send("User Created")
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtsecret'), {expiresIn: 360000}, (err, token)=>{
+            if (err) throw err;
+            res.status(200).json({ token })
+        })
     }
     catch(err){
         console.error(err.message)
