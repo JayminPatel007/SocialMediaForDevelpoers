b5ab6358578dcbeb044c318e64fe7829187daaea
discussion4all <discussion4all@yahoo.com>
2020-01-16 17:31:12 +0530

    Added Profile model and Route to fetch Current Users Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-
-// @route    GET api/profile
-// @desc     Test Route
-// @access   Public
-router.get("/", (req, res)=>res.send("Profile Route"));
+const Profile = require("../../models/Profile")
+const User = require("../../models/User");
+// @route    GET api/profile/me
+// @desc     Get Current Users Profile
+// @access   Private
+router.get("/me", auth , async (req, res)=>{
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+        if(!profile){
+            return res.status(400).json({
+                errors: [{
+                    msg: "There is no profile for this user"
+                }]
+            })
+        }
+        res.status(200).json(profile)
+    }catch(err){
+        console.error(err.message);
+        res.status(500).json({
+            errors: [
+                {
+                    msg: "Server Error"
+                }
+            ]
+        })
+    }
+});
 
 module.exports = router;
