0bc85a852be6f98ce27dd539119befaa60e96692
discussion4all <discussion4all@yahoo.com>
2020-01-16 16:04:35 +0530

    Added post User route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 //Connect Database
 connectDB();
-
+app.use(express.json({ extended: false}))
 app.get("/", (req, res)=>res.send("API is running"))
 
 app.use("/api/users", require("./routes/api/users"))
