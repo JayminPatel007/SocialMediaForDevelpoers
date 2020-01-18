414672eeb115d48d82e39fd57b7e08762c06dda9
discussion4all <discussion4all@yahoo.com>
2020-01-18 13:01:17 +0530

    Registratin using redux completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { setAlert } from '../../actions/alert'
 import PropTypes from 'prop-types'
+import {register} from '../../actions/auth'
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -23,7 +24,7 @@ const Register = ({setAlert}) => {
         if(password !== password2){
             setAlert('Passowrds do not match', "danger")
         } else{
-            console.log(formData)
+            register({name, email, password})
         }
     }
 
@@ -33,10 +34,10 @@ const Register = ({setAlert}) => {
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={e=> onSubmit(e)}>
                 <div className="form-group">
-                <input type="text" placeholder="Name" name="name" value={name} onChange={e=> onChange(e)} required/>
+                <input type="text" placeholder="Name" name="name" value={name} onChange={e=> onChange(e)}/>
                 </div>
                 <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)}/>
+                <input type="text" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)}/>
                 <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
                     Gravatar email</small
@@ -47,7 +48,6 @@ const Register = ({setAlert}) => {
                     type="password"
                     placeholder="Password"
                     name="password"
-                    minLength="6"
                     value={password}
                     onChange={e=>onChange(e)}
                 />
@@ -57,7 +57,6 @@ const Register = ({setAlert}) => {
                     type="password"
                     placeholder="Confirm Password"
                     name="password2"
-                    minLength="6"
                     value={password2}
                     onChange={e=>onChange(e)}
                 />
@@ -72,7 +71,8 @@ const Register = ({setAlert}) => {
 }
 
 Register.protoTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, {setAlert})(Register);
+export default connect(null, {setAlert, register})(Register);
--- client/src/components/layouts/Alert.js
+++ client/src/components/layouts/Alert.js
@@ -2,29 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-const Alert = ({alert}) => alert !== null && alert !== {} && (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
-)
-
-Alert.propTypes = {
-    alert: PropTypes.object.isRequired,
-}
-
-const mapStateToProp = state => ({
-    alert: state.alert
-})
-
-
-// const Alert = ({alerts}) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
-// <div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
-// ))
+// const Alert = ({alert}) => alert !== null && alert !== {} && (
+//     <div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
+// )
 
 // Alert.propTypes = {
-//     alerts: PropTypes.array.isRequired,
+//     alert: PropTypes.object.isRequired,
 // }
 
 // const mapStateToProp = state => ({
-//     alerts: state.alert
+//     alert: state.alert
 // })
+
+
+const Alert = ({alerts}) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
+<div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
+))
+
+Alert.propTypes = {
+    alerts: PropTypes.array.isRequired,
+}
+
+const mapStateToProp = state => ({
+    alerts: state.alert
+})
 
 export default connect(mapStateToProp)(Alert)
