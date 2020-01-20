cd12de62a209a804571d72a07b5b63b274348578
discussion4all <discussion4all@yahoo.com>
2020-01-20 12:06:15 +0530

    Sign In Log Out and other

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react'
-import { Link } from 'react-router-dom';
+import { Link , Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types'
+import {login} from '../../actions/auth'
 
-export const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -15,7 +18,11 @@ export const Login = () => {
 
     const onSubmit = e =>{
         e.preventDefault();
-        console.log(formData)
+        login({email, password})
+    }
+
+    if(isAuthenticated){
+        return <Redirect to="/dashboard" />
     }
 
     return (
@@ -35,7 +42,6 @@ export const Login = () => {
                     type="password"
                     placeholder="Password"
                     name="password"
-                    minLength="6"
                     value={password}
                     onChange={e=>onChange(e)}
                 />
@@ -48,3 +54,13 @@ export const Login = () => {
         </Fragment>
     )
 }
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+Login.protoTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
+export default connect(mapStateToProps, {login})(Login)
